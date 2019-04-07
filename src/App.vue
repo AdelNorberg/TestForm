20fbf3d180d17799f7a8cdee91dcf3b0b398d8aa
Adel <darzemanov101@gmail.com>
2019-04-07 22:30:37 +0300

    Fix scroll

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,10 @@
 </template>
 
 <style>
+#app {
+  max-width: 100%;
+}
+
 .link {
   margin: 0 1rem 0 1rem;
   font-size: 18px;
@@ -24,17 +28,16 @@
 
 .content {
   margin: 2rem;
-  min-width: 100%;
+  width: 100%;
 }
 
 .slide-fade-enter-active {
   transition: all .3s ease;
 }
 .slide-fade-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active до версии 2.1.8 */ {
+.slide-fade-enter, .slide-fade-leave-to {
   transform: translateX(10px);
   opacity: 0;
 }
